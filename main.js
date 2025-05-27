48a150e9f49e33995fba2d362d3b26314cfd275e
Malvika Rathod <115942210+malvika-rathod@users.noreply.github.com>
2025-05-27 23:39:28 +0530

    Add files via upload
    Electron js is used to deploy Desktop-version-of-Sanrio-Themed-Weather-App desktop application

---
+++ main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      contextIsolation: false,
+      nodeIntegration: true
+    }
+  });
+
+  win.loadFile(path.join(__dirname, 'index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
